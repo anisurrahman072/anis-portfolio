d0c1c3877545192f3f0bd59fbf831b39a596cb90
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-03-20 20:46:23 +0600

    Udated endorsements

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -27,21 +27,48 @@ function Home() {
                   textAlign: "left",
                 }}
               >
-                Sr. Software Engineer with{" "}
+                🟢 Sr. Software Engineer with{" "}
                 <b style={{ color: "gold" }}>5+ years</b> of JS{" "}
                 <b style={{ color: "gold" }}>Full Stack</b> experience.
               </h5>
 
-              <h5 style={{ paddingLeft: 50, textAlign: "left" }}>
-                🟡 Web & Mobile (Android & iOS)
-              </h5>
-              <h5 style={{ paddingLeft: 50, textAlign: "left" }}>
-                🟡 React, React Native, Node.js
+              <h5
+                style={{
+                  paddingLeft: 50,
+                  textAlign: "left",
+                }}
+              >
+                🟢 React, React Native & Node.js (
+                <b style={{ color: "gold" }}>MERN</b>) expert.
               </h5>
 
-              <h1 className="heading-name" style={{ marginTop: "50px" }}>
-                COMMUNITY <strong className="main-name">ENDORSED ME </strong>
+              <h1 className="endorse-title" style={{ marginTop: "60px" }}>
+                COMMUNITY <strong className="main-name">ENDORSED ME 🔥</strong>
               </h1>
+
+              <h5
+                style={{
+                  paddingTop: 50,
+                  paddingLeft: 50,
+                  paddingRight: 50,
+                  paddingBottom: 20,
+                  textAlign: "left",
+                }}
+              >
+                🟢 My BOOK was endorsed by Top{" "}
+                <b style={{ color: "gold" }}>Company</b> - ( Callstack )
+              </h5>
+
+              <h5
+                style={{
+                  paddingLeft: 50,
+                  textAlign: "left",
+                }}
+              >
+                🟢 I was featured on the Top{" "}
+                <b style={{ color: "gold" }}>RN Podcast</b> - ( by Infinite Red
+                )
+              </h5>
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
@@ -51,7 +78,7 @@ function Home() {
                 className="img-fluid book-pic"
                 style={{
                   maxHeight: "600px",
-                  borderRadius: "10px",
+                  borderRadius: "15px",
                   position: "relative",
                 }}
               />
