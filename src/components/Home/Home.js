f986ea292ccfc5c535ae3039ca31dbef4435f4c8
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-03-20 23:46:49 +0600

    Added endorsements

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,7 +20,7 @@ function Home() {
 
               <h5
                 style={{
-                  paddingTop: 50,
+                  paddingTop: 30,
                   paddingLeft: 50,
                   paddingRight: 50,
                   paddingBottom: 20,
@@ -42,46 +42,73 @@ function Home() {
                 <b style={{ color: "gold" }}>MERN</b>) expert.
               </h5>
 
-              <h1 className="endorse-title" style={{ marginTop: "60px" }}>
+              <h1 className="endorse-title" style={{ marginTop: "80px" }}>
                 COMMUNITY <strong className="main-name">ENDORSED ME 🔥</strong>
               </h1>
 
               <h5
                 style={{
-                  paddingTop: 50,
+                  paddingTop: 30,
                   paddingLeft: 50,
                   paddingRight: 50,
                   paddingBottom: 20,
                   textAlign: "left",
+                  position: "relative",
                 }}
               >
-                🎖️ My BOOK was endorsed by Top{" "}
-                <b style={{ color: "gold" }}>Company</b> - ( Callstack )
+                🎖️{" "}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://x.com/mdj_dev/status/1750908771713315326?s=20"
+                  className="custom-link cursor-pointer"
+                >
+                  My BOOK was <b style={{ color: "gold" }}>endorsed</b> by Top
+                  RN Company - ( Callstack ) 🔗
+                </a>
               </h5>
 
               <h5
                 style={{
                   paddingLeft: 50,
                   textAlign: "left",
+                  position: "relative",
                 }}
               >
-                🎖️ I was featured on the Top{" "}
-                <b style={{ color: "gold" }}>RN Podcast</b> - ( by Infinite Red
-                )
+                🎖️{" "}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://reactnativeradio.com/episodes/rnr-285-expo-dominates-the-app-store-and-other-news?t=23m1s"
+                  className="custom-link cursor-pointer"
+                >
+                  I was featured on the Top{" "}
+                  <b style={{ color: "gold" }}>RN Podcast</b> - ( by Infinite
+                  Red ) 🔗
+                </a>
               </h5>
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid book-pic"
-                style={{
-                  maxHeight: "600px",
-                  borderRadius: "15px",
-                  position: "relative",
-                }}
-              />
+              <div style={{ position: "relative" }}>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/anisurrahman072/React-Native-Advanced-Guide"
+                  className="custom-link cursor-pointer"
+                >
+                  <img
+                    src={homeLogo}
+                    alt="home pic"
+                    className="img-fluid book-pic"
+                    style={{
+                      maxHeight: "600px",
+                      borderRadius: "15px",
+                      position: "relative",
+                    }}
+                  />
+                </a>
+              </div>
             </Col>
           </Row>
         </Container>
