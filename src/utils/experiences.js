3619cfa529141cb21ab158ab23ef42a99a1acb14
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-03-29 19:18:50 +0600

    Removed extra job

--- src/utils/experiences.js
+++ src/utils/experiences.js
@@ -40,17 +40,4 @@ export default [
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
-  {
-    title: "Full stack Developer",
-    companyName: "Meta",
-    icon: basementsports,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
 ];
