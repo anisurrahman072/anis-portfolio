07937f89a9124fef4211d19f03fa09aed7120917
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-04-07 20:44:06 +0600

    Added nested virtualization

--- src/components/Blog/Blogs.js
+++ src/components/Blog/Blogs.js
@@ -369,6 +369,72 @@ function Blogs() {
                   </motion.div>
                 </a>
               </Col>
+
+              {/* BLOG RN NESTED VIRTUALIZATION */}
+              {/* BLOG RN NESTED VIRTUALIZATION */}
+              {/* BLOG RN NESTED VIRTUALIZATION */}
+              <Col
+                sm={6}
+                md={4}
+                style={{
+                  padding: "18px",
+                }}
+              >
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  className="custom-link cursor-pointer"
+                  href="https://medium.com/@anisurrahmanbup/react-native-nested-virtualization-anti-pattern-performance-optimization-958e98d4ea79"
+                >
+                  <motion.div
+                    className="blog-box"
+                    style={{
+                      borderRadius: "15px",
+                      textAlign: "left",
+                      position: "relative",
+                      overflow: "hidden",
+                      height: "100%",
+                      overflow: "hidden",
+                    }}
+                    onHoverStart={handleHoverBsport}
+                    onHoverEnd={handleHoverBsport}
+                    variants={{
+                      hidden: {
+                        x: -100,
+                        opacity: 0,
+                      },
+                      show: {
+                        x: 0,
+                        opacity: 1,
+                        transition: {
+                          type: "spring",
+                          duration: 1.25,
+                          delay: 0,
+                        },
+                      },
+                    }}
+                  >
+                    <div>
+                      <motion.div
+                        initial={{ scale: 1 }} // Initial state: very small and invisible
+                        animate={{
+                          scale: isHoveredBsport ? 1.04 : 1, // Zoom in
+                        }}
+                        transition={{ duration: 0.45 }} // Animation duration (in seconds)
+                        className="blog-image"
+                      >
+                        <img
+                          src={
+                            "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*ut1Fr0oJhGpHGaC2wTXd_Q.png"
+                          }
+                          alt="home pic"
+                          className="img-fluid"
+                        />
+                      </motion.div>
+                    </div>
+                  </motion.div>
+                </a>
+              </Col>
             </Row>
           </Container>
         </motion.section>
