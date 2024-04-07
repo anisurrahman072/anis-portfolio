361562a11ccce13b61bbd45d345db172ac09d092
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-04-07 15:13:05 +0600

    removed unnecessary

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -17,20 +17,6 @@ function Testimonials() {
   // STATE for mobile device
   const [isMobile, setIsMobile] = useState(false);
 
-  // Hover STATES
-  const [isHoveredBsport, setIsHoveredBsport] = useState(false);
-  const [isHoveredEcommerce, setIsHoveredEcommerce] = useState(false);
-  const [isHoveredNft, setIsHoveredNft] = useState(false);
-  const [isHoveredYouth, setIsHoveredYouth] = useState(false);
-  const [isHoveredTruck, setIsHoveredTruck] = useState(false);
-
-  // STATE for checking Hover
-  const [isHoveredBook, setIsHoveredBook] = useState(false);
-
-  const handleHoverBook = () => {
-    setIsHoveredBook(!isHoveredBook);
-  };
-
   // Check if the device is a mobile or not (Assuming less than 767 is a MOBILE)
   useEffect(() => {
     const handleResize = () => {
@@ -48,39 +34,6 @@ function Testimonials() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-
-  const handleHoverBsport = () => {
-    setIsHoveredBsport(!isHoveredBsport);
-  };
-
-  const handleHoverEcommerce = () => {
-    setIsHoveredEcommerce(!isHoveredEcommerce);
-  };
-
-  const handleHoverNft = () => {
-    setIsHoveredNft(!isHoveredNft);
-  };
-
-  const handleHoverYouth = () => {
-    setIsHoveredYouth(!isHoveredYouth);
-  };
-
-  const handleHoverTruck = () => {
-    setIsHoveredTruck(!isHoveredTruck);
-  };
-
-  const images = [
-    "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80",
-    "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80",
-    "https://media.istockphoto.com/photos/the-main-attraction-of-paris-and-all-of-europe-is-the-eiffel-tower-in-picture-id1185953092?k=6&m=1185953092&s=612x612&w=0&h=SNiShskOfwQ7Sys5TX0eb5eBxHobktWUfZGrox5LMyk=",
-    "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
-    "https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270",
-    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-    "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-  ].map((image) => ({
-    id: crypto.randomUUID(),
-    image,
-  }));
 
   return (
     <Container className="leadershipContainer">
