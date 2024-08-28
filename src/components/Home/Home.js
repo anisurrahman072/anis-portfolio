cadf9e208f5340cedc36aee2f5f0426053c7f2ec
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-08-28 14:39:35 +0600

    Updated intro

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -83,7 +83,7 @@ function Home() {
                 transition={{ duration: 2 }} // Animation duration
               >
                 <h1 className="heading-name">
-                  🙋‍♂️ I'M
+                  <strong>I'M</strong>
                   <strong className="main-name"> ANISUR RAHMAN </strong>
                 </h1>
 
